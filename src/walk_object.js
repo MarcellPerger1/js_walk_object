83d4975585cb43e485e288e37c3bbcd71df3f1c8
Marcell Perger <marcell.perger.1@gmail.com>
2023-12-17 22:10:47 +0000

    docs: Add JSDoc

--- src/walk_object.js
+++ src/walk_object.js
@@ -1,4 +1,12 @@
 // For now, these just console.log the paths
+/**
+ * console.log all of the (deep) keys on `obj` that match `regexQuery` using a recursive DFS method
+ * @param {Object} obj The object
+ * @param {RegExp} regexQuery Match the key (not the full path)
+ * @param {string} rootPath The name of the root node
+ * @param {Set<Object>} _memo private
+ * @returns 
+ */
 export function walk_dfs(obj, regexQuery, rootPath = "<root>", _memo = null) {
     if (obj !== Object(obj)) return;
     if ((_memo ??= new Set).has(obj)) return;
@@ -15,6 +23,14 @@ export function walk_dfs(obj, regexQuery, rootPath = "<root>", _memo = null) {
 const NOT_IN_PATHMAP = "Encountered path that was not in pathMap. " +
     "Please report this as an issue to https://github.com/MarcellPerger1/js_walk_object if it hasn't been reported already";
 
+/**
+ * console.log all of the (deep) keys on `obj` that match `regexQuery` using an iterative BFS method
+ * @param {Object} obj The object
+ * @param {RegExp} regexQuery Match the full path (not the key)
+ * @param {string} rootPath The name of the root node
+ * @param {Set<Object>} _memo private
+ * @returns 
+ */
 export function walk_bfs(startObj, regexQuery, rootPath = "<root>") {
     let openQueue = new Set([startObj]), closed = new Set;
     let pathMap = new Map([[startObj, rootPath]]);
